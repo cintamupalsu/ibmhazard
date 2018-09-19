8e80a2030772e681915935d1f1fb3bbbb3bbd14e
maulanamania <maulanamania@gmail.com>
2018-09-19 00:17:51 +0000

    Region data seed finish

--- app/controllers/datamanager_controller.rb
+++ app/controllers/datamanager_controller.rb
@@ -1,20 +1,23 @@
 class DatamanagerController < ApplicationController
   
   def region
-  
+    @regions = Region.all
   end
   
   def seedregion
     varTxt=params[:fileTXT]
     fTxt=open(varTxt.path,"r:UTF-8")
     until fTxt.eof do
-      dline=fTxt.gets.chomp.split(' ')
+      dline=fTxt.gets.chomp.split('|')
+      if dline[4]!=nil
+        #Region.create(lat:dline[0].to_f, lon:dline[1].to_f, x:dline[2].to_i, y:dline[3].to_i, name:dline[4])      
+      end
     end
     fTxt.close()
   end
   
   def deleteallregion
-    Region.delete_all
+    # Region.delete_all
     @message = "Its done"
   end
 end
